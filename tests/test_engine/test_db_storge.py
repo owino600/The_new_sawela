71831f08bef6839e68f4f031f9bafeb4ad6a432e
owino600 <owinodaniel600@gmail.com>
2024-06-08 18:55:04 +0300

    update

--- tests/test_engine/test_db_storge.py
+++ tests/test_engine/test_db_storge.py
@@ -7,20 +7,12 @@ from datetime import datetime
 import inspect
 import models
 from models.engine import db_storage
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 import json
 import os
 import pep8
 import unittest
 DBStorage = db_storage.DBStorage
-classes = {"Amenity": Amenity, "City": City, "Place": Place,
-           "Review": Review, "State": State, "User": User}
+classes = {}
 
 
 class TestDBStorageDocs(unittest.TestCase):
