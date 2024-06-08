9593706f0d0232731176e0f29713bb94fd7f9cda
owino600 <owinodaniel600@gmail.com>
2024-06-08 17:57:27 +0300

    db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,49 +1,18 @@
 #!/usr/bin/pythom3
-"""New engine DBStorage: (models/engine/db_storage.py)
-
-Private class attributes:
-__engine: set to None
-__session: set to None
-Public instance methods:
-__init__(self):
-create the engine (self.__engine)
-the engine must be linked to the MySQL database and user created before (hbnb_dev and hbnb_dev_db):
-dialect: mysql
-driver: mysqldb
-all of the following values must be retrieved via environment variables:
-MySQL user: HBNB_MYSQL_USER
-MySQL password: HBNB_MYSQL_PWD
-MySQL host: HBNB_MYSQL_HOST (here = localhost)
-MySQL database: HBNB_MYSQL_DB
-don’t forget the option pool_pre_ping=True when you call create_engine
-drop all tables if the environment variable HBNB_ENV is equal to test
-all(self, cls=None):
-query on the current database session (self.__session) all objects depending of the class name (argument cls)
-if cls=None, query all types of objects (User, State, City, Amenity, Place and Review)
-this method must return a dictionary: (like FileStorage)
-key = <class-name>.<object-id>
-value = object
-new(self, obj): add the object to the current database session (self.__session)
-save(self): commit all changes of the current database session (self.__session)
-delete(self, obj=None): delete from the current database session obj if not None
-reload(self):
-create all tables in the database (feature of SQLAlchemy) (WARNING: all classes who inherit from Base must be imported before calling Base.metadata.create_all(engine))
-create the current database session (self.__session) from the engine (self.__engine) by using a sessionmaker - the option expire_on_commit must be set to False ; and scoped_session - to make sure your Session is thread-safe"""
+""" ke sure your Session is thread-safe"""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 from os import getenv
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
+from models. import
+from models. import
+from models. import
+from models. import
+from models. import
+from models. import
 Base = declarative_base()
-classes = {"BaseModel": BaseModel, "User": User, "State": State,
-           "City": City, "Amenity": Amenity, "Place": Place,
-           "Review": Review}
+classes = {"BaseModel": BaseModel}
 class DBStorage:
     """DBStorage class"""
     __engine = None
