724cc8eb8246eaa6499cb0db7e01f4d4bec715fb
owino600 <owinodaniel600@gmail.com>
2024-06-08 18:57:26 +0300

    update

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -7,20 +7,13 @@ from datetime import datetime
 import inspect
 import models
 from models.engine import file_storage
-from models.amenity import Amenity
-from models.base_model import BaseModel
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
 import json
+from models.base_model import BaseModel
 import os
 import pep8
 import unittest
 FileStorage = file_storage.FileStorage
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
+classes = {"BaseModel": BaseModel}
 
 
 class TestFileStorageDocs(unittest.TestCase):
