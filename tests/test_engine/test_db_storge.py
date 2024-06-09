91c7e2d502fe5ac186892aadf4ddf9ca53a0ed15
owino600 <owinodaniel600@gmail.com>
2024-06-09 18:19:49 +0300

    updaate

--- tests/test_engine/test_db_storge.py
+++ tests/test_engine/test_db_storge.py
@@ -32,8 +32,8 @@ class TestDBStorageDocs(unittest.TestCase):
         """Test tests/test_models/test_db_storage.py conforms to PEP8."""
         pep8s = pycodestyle.StyleGuide(quiet=True)
         result = pep8s.check_files(['tests/test_engine/test_db_storage.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warnings).")
 
     def test_db_storage_module_docstring(self):
         """Test for the db_storage.py module docstring"""
