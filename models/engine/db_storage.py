1f7b7a4f328323c4069032b22adf45f572d1c279
Daniel Owino <135459704+owino600@users.noreply.github.com>
2024-07-12 13:39:57 +0300

    Update db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,15 +1,14 @@
-#!/usr/bin/pythom3
-""" ke sure your Session is thread-safe"""
+#!/usr/bin/python3
+"""Ensure your Session is thread-safe"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 from os import getenv
 from models.base_model import BaseModel
 
-
 Base = declarative_base()
 classes = {"BaseModel": BaseModel}
-
 
 class DBStorage:
     """DBStorage class"""
@@ -24,26 +23,28 @@ class DBStorage:
         STORE_MYSQL_HOST = getenv('STORE_MYSQL_HOST')
         STORE_MYSQL_DB = getenv('STORE_MYSQL_DB')
         STORE_ENV = getenv('HBNB_ENV')
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
-            STORE_MYSQL_USER,
-            STORE_MYSQL_PWD,
-            STORE_MYSQL_HOST,
-            STORE_MYSQL_DB
-            ),
-                                      pool_pre_ping=True)
+
+        if None in (STORE_MYSQL_USER, STORE_MYSQL_PWD, STORE_MYSQL_HOST, STORE_MYSQL_DB):
+            raise ValueError("Database configuration environment variables are not set")
+
+        self.__engine = create_engine(
+            f"mysql+mysqlconnector://{STORE_MYSQL_USER}:{STORE_MYSQL_PWD}@{STORE_MYSQL_HOST}/{STORE_MYSQL_DB}",
+            pool_pre_ping=True
+        )
+
         if STORE_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """query on the current database session"""
+        """Query on the current database session"""
         new_dict = {}
         for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
+            if cls is None or cls in {classes[clss], clss}:
                 objs = self.__session.query(classes[clss]).all()
                 for obj in objs:
-                    key = obj.__class__.__name__ + '.' + obj.id
+                    key = f"{obj.__class__.__name__}.{obj.id}"
                     new_dict[key] = obj
-        return (new_dict)
+        return new_dict
 
     def new(self, obj):
         """Add the object to the current database session"""
@@ -60,9 +61,7 @@ class DBStorage:
             self.save()
 
     def reload(self):
-        """Create all tables in the database (feature of SQLAlchemy)
-        (WARNING: all classes who inherit from Base must be imported
-        before calling Base.metadata.create_all(engine))"""
+        """Create all tables in the database"""
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
@@ -73,17 +72,13 @@ class DBStorage:
         self.__session.remove()
 
     def get(self, cls, id):
-        """
-        Get an object based on class name and its ID
-        """
+        """Get an object based on class name and its ID"""
         if cls not in classes:
             return None
         return self.__session.query(classes[cls]).get(id)
 
     def count(self, cls=None):
-        """
-        Count the number of objects in storage
-        """
+        """Count the number of objects in storage"""
         if cls is None:
             return sum(len(self.all(c)) for c in classes)
         if cls not in classes:
