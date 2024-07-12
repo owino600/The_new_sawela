18a2759c7c8fbd59888e8a4a496fdcf91bd36920
owino600 <owinodaniel600@gmail.com>
2024-07-12 21:46:40 +0300

    .\__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,10 @@ initialize package to create a
 unique FileStorage instance
 """
 from os import getenv
+from models.base_model import BaseModel
+from models.stock_received import StockReceived
+from models.stock_issued import StockIssued
+from models.inventory import Inventory
 
 storage_t = getenv('STORE_TYPE_STORAGE')
 from models.engine.db_storage import DBStorage
